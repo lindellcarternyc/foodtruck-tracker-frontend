7b614d3159548a9aa2ebebf5cee90688df31efa8
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-31 19:08:36 -0400

    return truck data from api response

--- src/api-client/trucks.js
+++ src/api-client/trucks.js
@@ -3,7 +3,7 @@ import { axiosWithAuth } from './axiosWithAuth'
 export const fetchTrucks = async () => {
   try {
     const response = await axiosWithAuth().get(`/api/trucks/trucks`)
-    console.log(response)
+    return response.data
   } catch (err) {
     console.log(err)
     throw err
