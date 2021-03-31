64161afccb0d753e49f0ece19c0b90feae38399a
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-31 19:37:23 -0400

    update operator dashboard

--- src/pages/HomePage/OperatorDashboard.js
+++ src/pages/HomePage/OperatorDashboard.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { fetchTrucksById } from '../../api-client'
+
 const OperatorDashboard = () => {
+  const currentUser = useSelector(state => state.userState.user)
+  const [operatorTrucks, setOperatorTrucks] = useState([])
+
+  useEffect(() => {
+    const truckIDs = currentUser.trucks.map(entry => {
+      return entry.truck.truckid
+    })
+    fetchTrucksById(truckIDs)
+      .then(trucks => {
+        setOperatorTrucks(trucks)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [currentUser])
+
   return (
     <div>
       <h2>Operator Dashboard</h2>
+      <div>
+        <h3>Operator's Trucks</h3>
+        <p>{JSON.stringify(operatorTrucks)}</p>
+      </div>
     </div>
   )
 }
