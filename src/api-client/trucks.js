1aa1bc1b9fa9a5c680e410699b3a5dc8ad97a117
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-31 19:35:35 -0400

    update api client trucks

--- src/api-client/trucks.js
+++ src/api-client/trucks.js
@@ -5,11 +5,25 @@ export const fetchTrucks = async () => {
     const response = await axiosWithAuth().get(`/api/trucks/trucks`)
     return response.data
   } catch (err) {
-    console.log(err)
     throw err
   }
 }
 
-export const fetchTruckById = async () => {
+export const fetchTruckById = async (truckID) => {
+  try {
+    const response = await axiosWithAuth().get(`/api/trucks/truck/${truckID}`)
+    return response.data
+  } catch (err) {
+    throw err
+  }
+}
 
+export const fetchTrucksById = async (truckIDs) => {
+  try {
+    const truckRequests = truckIDs.map(fetchTruckById)
+    const trucks = await Promise.all(truckRequests)
+    return trucks
+  } catch (err) {
+    throw err
+  }
 }
