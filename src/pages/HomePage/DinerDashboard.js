23d4217ba8f5adc9b42d5a27420c00686e53f79f
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-31 09:35:28 -0400

    refactor home page

---
+++ src/pages/HomePage/DinerDashboard.js
@@ -0,0 +1,9 @@
+const DinerDashboard = () => {
+  return (
+    <div>
+      <h2>Diner Dashboard</h2>
+    </div>
+  )
+}
+
+export default DinerDashboard
---
+++ src/pages/HomePage/OperatorDashboard.js
@@ -0,0 +1,9 @@
+const OperatorDashboard = () => {
+  return (
+    <div>
+      <h2>Operator Dashboard</h2>
+    </div>
+  )
+}
+
+export default OperatorDashboard
