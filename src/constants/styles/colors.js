c6cd04896478d92271f42f50a2527afbaeec04f6
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-31 07:04:06 +0100

    existing color constants added

---
+++ src/constants/styles/colors.js
@@ -0,0 +1,12 @@
+export const BLACK = '#000000'          // Color of most text, button background or outline, nav bar background
+export const WHITE = '#FFFFFF'          // Page background, color of button text, nav text/icons
+
+export const DISABLED_GRAY = '#4C4C4C'  // Color of disabled buttons
+export const ERROR_RED = '#FF3300'      // Color of error text/input error borders
+
+// INPUT FIELDS
+export const INPUT_ORANGE = '#FFDDCC'       // Default input field background
+
+export const LIGHT_INPUT_ORANGE = '#FFF5F0' // Prefilled input field background
+export const DARK_INPUT_ORANGE = '#FFCCB3'  // Prefilled input field border
+export const INPUT_TEXT_GRAY = '#333333'    // Prefilled input field text color
