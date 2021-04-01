c25ca99e8b6f7e258b472d1d18c4dd34ca1c53ca
Jack Henry <jackson.lewis.henry@gmail.com>
2021-04-01 12:25:24 -0400

    homepage display fixed

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,18 +1,30 @@
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import * as ROUTES from '../../constants/routes'
 import * as USER_ROLES from '../../constants/user-roles'
+
+import Container from '../../components/styled/Container'
+import Button from '../../components/styled/Button'
 
 import DinerDashboard from './DinerDashboard'
 import OperatorDashboard from './OperatorDashboard'
 
 const HomePage = ({ currentUser }) => {
+
+  const history = useHistory()
+
+  const routeTo = (location) => {
+      history.push(location)
+  }
+
   if (!currentUser) {
     return (
       <div>
+        <Container>
         <h2>Welcome to Food Truck TrackR</h2>
-        <Link to={ROUTES.LOGIN}>LOGIN</Link>
-        <Link to={ROUTES.SIGNUP}>SIGNUP</Link>
+                    <Button onClick={() => routeTo('/login')}>Log In</Button>
+                    <Button onClick={() => routeTo('/signup')}>Sign Up</Button>
+        </Container>            
       </div>
     )
   } else if (currentUser.role === USER_ROLES.DINER) {
