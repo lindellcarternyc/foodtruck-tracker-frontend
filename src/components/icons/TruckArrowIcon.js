acef72e6a1f6a8fde2cc908b52a860ff2748c806
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-01 17:49:10 +0100

    TruckArrowIcon

---
+++ src/components/icons/TruckArrowIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTruck, faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+
+export default function TruckArrowIcon(props) {
+    const { size } = props;
+
+    return (
+        <div className={`truck-search fa-layers fa-${size}`}>
+            <FontAwesomeIcon icon={faLocationArrow} mask={faTruck} transform='shrink-8 up-2 left-3.75'/>
+        </div> 
+    )
+}
