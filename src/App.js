d7eea69ab9bb1a3cba507502d37a151b3db4ebce
Lindell Carter <lindellcarternyc@gmail.com>
2021-04-01 12:10:57 -0400

    remove link and add basic view truck page functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
