c97bcbc83b7d7ebb70bc154c8fdc8567d924ae3d
Jack Henry <jackson.lewis.henry@gmail.com>
2021-03-31 20:19:44 -0400

    create and edit menuitems form/pages

--- src/pages/CreateMenuItemPage.js
+++ src/pages/CreateMenuItemPage.js
@@ -1,7 +1,16 @@
+// import { useDispatch, useSelector } from 'react-redux'
+// import EditUserForm from '../forms/EditUserForm'
+// import { updateUser } from '../store/features/user'
+
+import CreateMenuItemForm from '../forms/CreateMenuItemForm'
+import Container from '../components/styled/Container'
+
 const CreateMenuItemPage = () => {
   return (
     <div>
-      <h2>Create Menu Item</h2>
+      <Container>
+        <CreateMenuItemForm/>
+      </Container>
     </div>
   )
 }
--- src/pages/UserInfoPage.js
+++ src/pages/UserInfoPage.js
@@ -1,16 +1,37 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
+import * as ROUTES from '../constants/routes'
+
+
+import Button from '../components/styled/Button'
+import Container from '../components/styled/Container'
 
 
 const UserInfoPage = () => {
   const user = useSelector(state => state.userState.user)
 
+  const history = useHistory()
+
+  const routeTo = (location) => {
+    history.push(location)
+  }
+  const logout = () => {
+    localStorage.removeItem('food-tracker-token');
+    window.location.reload();
+  };
+  
   return (
     <div>
-      <h2>User Info</h2>
-      <h2>Username: {user.username}</h2>
-      <h2>Email: {user.email}</h2>
-      <h2>Role: {user.role}</h2>
+      <Container>
+        <h2>User Info</h2>
+        <h2>Username: {user.username}</h2>
+        <h2>Email: {user.email}</h2>
+        <h2>Role: {user.role}</h2>
+        <Button type="button" onClick={logout}>Logout</Button>
+        <Button type="button" onClick={()=> routeTo(ROUTES.EDIT_USER)}>Edit User</Button>
+      </Container>
     </div>
   )
 }
