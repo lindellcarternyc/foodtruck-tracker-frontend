8463009edc73e522b5cf9cff87d3acfd1b78659d
Lindell Carter <lindellcarternyc@gmail.com>
2021-04-02 05:51:52 -0400

    add store data to favorites page

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,9 +1,11 @@
-
-
-const FavoritesPage = () => {
+const FavoritesPage = ({ currentUser }) => {
   return (
     <div>
       <h1>Favorites</h1>
+      {currentUser.trucks.map(t => {
+        // This is the shape of the data being returned from the api
+        return <p key={t.truck.truckid}>{JSON.stringify(t)}</p>
+      })}
     </div>
   )
 }
